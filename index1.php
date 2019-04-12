dd581e47a67140566789b377fe39320ff7b60463
Suramya <suramyagupta2@gmail.com>
2019-04-12 20:49:28 +0530

    add index1.php

---
+++ index1.php
@@ -0,0 +1,31 @@
+<?php
+require "/opt/lampp/htdocs/Header.php";
+?>
+<main>
+<div class="wrapper-main">
+	<section class="section-default">
+	<?php
+	if (isset($_SESSION['userid'])) 
+	{
+		echo '<p class="login-status">You are Logged in!</p>';
+	}
+	else
+	{
+		echo '<p class="login-status">You are Logged out! </p>';
+
+	}
+		
+	?>	
+	</section>
+</div>
+	
+</main>
+<?php
+require "footer.php";
+?>
+
+
+
+
+
+
